327bd58f9e4132b34d11d3687bd5c4a97851a238
Pauline L <paulinelauron@gmail.com>
2018-06-19 15:53:57 +0200

    neat-python package

--- neat_python.py
+++ neat_python.py
@@ -4,30 +4,41 @@ Created on Tue Jun 19 13:31:39 2018
 
 @author: Pauline LAURON
 """
-from flappybird.game import FlappyGame
+from flappybird.game import FlappyGame, normalize_state
 import neat
+import visualize 
 
 MAX_FITNESS = 0
 BEST_GENOME = None
+GENERATION = 0
 
 def game(genome, config):
     
     net = neat.nn.FeedForwardNetwork.create(genome, config)
     
-    bird = FlappyGame(return_rgb=False, display_screen=False)
-    fitness = 0 
+    bird = FlappyGame(return_rgb=False, display_screen=False, reward_clipping=True)
+    fitness = 1
     
-    state = bird.get_game()
+    state = normalize_state(bird.get_state())
+    actions = bird.game.getActionSet()
     
     while True: 
         
         output = net.activate(state)
         
-        reward, state, done = bird.do_action()
+        if output[0] >= 0.5:
+            #up
+            reward, state, done = bird.do_action(actions[0])
+        else :
+            #do nothing
+            reward, state, done = bird.do_action(actions[1])
+            
+        state = normalize_state(state)
         
         fitness += reward
         
         if done :
+            state = bird.reset()
             break        
     
     return fitness
@@ -35,30 +46,72 @@ def game(genome, config):
         
 
 def eval_genomes(genomes, config):
-    global MAX_FITNESS, BEST_GENOME
-    
+    global MAX_FITNESS, BEST_GENOME, GENERATION
+     
+    i = 0
     for genome_id, genome in genomes:
         #genome.fitness = 0 
         genome.fitness = game(genome, config)
-        
+        #print("Gen : %d Genome # : %d  Fitness : %f Max Fitness : %f"%(GENERATION,i,genome.fitness, MAX_FITNESS))
         if genome.fitness >= MAX_FITNESS:
             MAX_FITNESS = genome.fitness
             BEST_GENOME = genome
+        i += 1
+    GENERATION += 1
 
 def run(config_file):
     # Load configuration.
-    config = neat.Config.Config(neat.DefaultGenome, neat.DefaultReproduction,
+    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
                          neat.DefaultSpeciesSet, neat.DefaultStagnation,
-                         'config_file')
+                         config_file)
     
     # Create the population, which is the top-level object for a NEAT run.
     p = neat.Population(config)
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+    p.add_reporter(neat.Checkpointer(5))
     
     # Run for up to 300 generations.
-    winner = p.run(eval_genomes, 300)
+    winner = p.run(eval_genomes, 30)
     
     # Display the winning genome.
     print('\nBest genome:\n{!s}'.format(winner))
+    
+    winner_net = neat.nn.FeedForwardNetwork.create(winner, config)
+    
+    bird = FlappyGame(return_rgb=False, display_screen=False, reward_clipping=True)
+    fitness = 1
+    
+    state = normalize_state(bird.get_state())
+    actions = bird.game.getActionSet()
+    
+    while True: 
+        
+        output = winner_net.activate(state)
+        
+        if output[0] >= 0.5:
+            #up
+            reward, state, done = bird.do_action(actions[0])
+        else :
+            #do nothing
+            reward, state, done = bird.do_action(actions[1])
+            
+        state = normalize_state(state)
+        
+        fitness += reward
+        
+        if done :
+            state = bird.reset()
+            break        
+        
+    visualize.draw_net(config, winner, True)
+    visualize.plot_stats(stats, ylog=False, view=True)
+    visualize.plot_species(stats, view=True)
+    
+    
+    #p.run(eval_genomes, 10)
+    
 
     
 if __name__ == '__main__':
